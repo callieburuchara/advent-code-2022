eeef04da681479e72b5e35aefea9ba9f88cc3721
Callie Buruchara <cburuchara@newrelic.com>
2022-12-01 15:42:49 -0500

    Initial commit

---
+++ Day1/Part1.ts
@@ -0,0 +1,22 @@
+// Source: https://adventofcode.com/2022/day/1
+
+/*
+- Input is a list of numbers, always valid
+  - Visually, looks like there are no stray spaces
+- Elves are separated by newlines
+  - Each elf's calories are the sum of its partitioned numbers
+- Goal: find the highest amount of summed calories (belonging to one single elf)
+
+- Separate, then sum, then sort, then you have the largest
+- *Sum as we separate by newslines, always keep track of the largest, replace largest as it gets beat, present largest after looking at every elf
+
+- How to import from .txt file into TS?
+*/
+
+const path = require('path');
+const fs = require('fs');
+
+
+const input = fs.readFileSync(path.join(__dirname, './Part1Input.txt'), 'utf-8');
+const byElf = input.split('\n\n');
+console.log(input.split('\n\n'))
