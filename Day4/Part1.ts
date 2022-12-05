0ed6c1a39c2e136fc5fe2f4a83c58fea7a1b503d
Callie Buruchara <cburuchara@newrelic.com>
2022-12-04 22:00:14 -0500

    Day 4 completed

---
+++ Day4/Part1.ts
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf-8');
+
+/*
+- input: string numbers, pairs of ranges: xx-xx, xx-xx (could be single digit or double digit)
+- count how many pairs there are where one in a pair completely encompasses the other
+  - the second could encompass the first or vice versa
+  - maybe use a <= && >=
+- Remember to convert them from strings to numbers before comparing them
+*/
+
+const allPairs = input.split('\n');
+let pairCount: number = 0;
+
+allPairs.forEach((pair: string) => {
+  const [first, second] = pair.split(',')
+  const [first1, second1] = first.split('-').map(Number);
+  const [first2, second2] = second.split('-').map(Number);
+
+  if ((first1 <= first2 && second1 >= second2) ||
+       (first2 <= first1 && second2 >= second1)) {
+        pairCount += 1;
+       }
+})
+
+console.log(pairCount);
+
+export {};
